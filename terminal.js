4ecf09c9de5b2df03f7add7f6f063aee23b77b23
dince12 <danielince72@hotmail.com>
2018-07-07 22:51:49 +0100

    Terminal script
    Code for terminal outputs and JSON

---
+++ terminal.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const request = require('request');
+var cowsay = require("cowsay");
+var colors = require('colors');
+var number =  0.5 + (Math.random() * ((250 - 1) + 1));
+var numberUse = Math.round(number);
+ request('http://numbersapi.com/' + numberUse + '/trivia', { json: false }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(cowsay.say({
+	text : body,
+	e : "oO",
+	T : "U "
+}).rainbow);
+
+});
+//Enjoy xx 
